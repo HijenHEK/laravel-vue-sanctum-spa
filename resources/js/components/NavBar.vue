ba4e1c426bb5870a268e6d14e1eb5d7d923ee282
HijenHek <k.hijen@gmail.com>
2021-04-23 14:52:04 +0100

    navbar drop + heroicons

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -5,14 +5,21 @@
                 Laravel Vue
             </div>
 
-            <div class="relative">
-                <div @click="drop=!drop" class=" cursor-pointer p-4  font-semibold tracking-wider text-lg"> Foulen </div>
+            <div class="relative "  ref="dropMenu">
+                <div @click="drop=!drop"  class=" flex items-center cursor-pointer p-4  font-semibold tracking-wider text-lg"> 
+                Foulen 
+                <ChevronDownIcon class="h-5 w-5 text-gray-700 ml-2 mt-1"></ChevronDownIcon>
+
+                </div>
 
                 <div v-if="drop" class="absolute bg-white border z-10 shadow-md flex w-auto flex-col ">
-                    <router-link class="p-4 " :to="{ name: 'login' }">
+                    <router-link class="p-4 flex items-center" :to="{ name: 'login' }">
+                        <CogIcon class="h-6 w-6 text-gray-700 mr-2"></CogIcon>
                         Settings
                     </router-link>
-                    <div class="p-4 ">
+                    <div class="p-4 flex  items-center cursor-pointer">
+                        <LogoutIcon class="h-6 w-6 text-gray-700 mr-2"></LogoutIcon>
+
                         Logout
                     </div>
                 </div>
@@ -21,7 +28,20 @@
     </div>
 </template>
 <script>
+import { CogIcon , LogoutIcon , ChevronDownIcon } from '@heroicons/vue/outline'
+
+
 export default {
+    created: function() {
+        let self = this ;   
+        window.addEventListener('click', function(e){
+            if (! self.$refs.dropMenu.contains(e.target) ){
+                self.drop = false
+            } 
+        })
+    },
+
+    components: { CogIcon , LogoutIcon , ChevronDownIcon } ,
     data() {
         return {
             drop : false
