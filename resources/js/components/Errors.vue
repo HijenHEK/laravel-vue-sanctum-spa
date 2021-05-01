419310c3dd481f08bd8685bb7537da3e3a456d73
HijenHek <k.hijen@gmail.com>
2021-05-01 23:14:04 +0100

    error enhance

--- resources/js/components/Errors.vue
+++ resources/js/components/Errors.vue
@@ -1,11 +1,11 @@
 <template >
   <div class="flex items-start border px-4 py-3 rounded relative md:w-10/12 md:p-2 w-full mx-auto bg-red-100 border border-red-100 text-red-500 " role="alert shadow">
                             <!-- <strong class="font-bold">Holy smokes!</strong> -->
-                            <span v-if="!content.errors" class="block sm:inline w-full text-center">{{content}}</span>
+                            <span v-if="!content.errors" class="block sm:inline w-full text-center">{{content.message}}</span>
                             <div v-else class="block sm:inline w-full text-center">
                             
-                            <div>{{content.message}}</div>
-                             <ul class="text-sm mt-2">
+                            <!-- <div>{{content.message}}</div> -->
+                             <ul class="text-sm ">
                                     <li v-for="e in content.errors" :key="e.index">
                                         {{e[0]}}
                                     </li>
